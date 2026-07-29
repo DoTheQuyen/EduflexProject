2f0dff67cbf47b7f8e86249fd8307d14b1f78000
quyendo <quyen.galiso@gmail.com>
2026-07-29 16:51:47 +0700

    update footer

--- eduflex-Angular/src/app/components/public/footer/footer.component.ts
+++ eduflex-Angular/src/app/components/public/footer/footer.component.ts
@@ -3,6 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 
+interface FooterSocialLink {
+  label: string;
+  icon: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -12,4 +18,11 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+  socialLinks: FooterSocialLink[] = [
+    { label: 'Facebook', icon: 'fab fa-facebook-f', url: '' },
+    { label: 'Instagram', icon: 'fab fa-instagram', url: '' },
+    { label: 'LinkedIn', icon: 'fab fa-linkedin-in', url: '' },
+    { label: 'YouTube', icon: 'fab fa-youtube', url: '' }
+  ];
 }
